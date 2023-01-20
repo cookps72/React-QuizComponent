496e5d97005b4c4f46f7a68d23da4cc74a68c38e
cookp <cookp@slc.co.uk>
2023-01-20 13:11:28 +0000

    lesson1

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,12 +2,13 @@ import React, {Component} from "react";
 
 class QuizQuestion extends React.Component {
 
+
     render(){
 
         return (
             <main>
                 <section>
-                    <p>this.props.quiz_question.instruction_text</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
