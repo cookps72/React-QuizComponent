aca5b510b51584560a7bbbe04b0a54f35ee1dfe6
cookp <cookp@slc.co.uk>
2023-01-20 11:19:08 +0000

    lesson1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,20 @@
 import React, {Component} from "react";
 
-let quizDate = require ('./quiz_data.json');
+let quizData = require('./quiz_data.json');
+
+class Quiz extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position : 1};
+    }
+
+
+    render() {
+        return (<div>Quiz</div>);
+
+    }
+}
+
+export default Quiz;
 
